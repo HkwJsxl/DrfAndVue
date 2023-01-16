2449b2242aaddf5376cc4cf9029e1ca9ebf1536d
韩科伟 <hkwJsxl@gmail.com>
2023-01-16 10:31:26 +0800

    update vue

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import Vue from 'vue'
 
 createApp(App).mount('#app')
+
+
+// 配置全局样式
+import '@/assets/css/global.css'
+
+// 配置全局自定义设置
+import settings from '@/assets/js/settings'
+
+Vue.prototype.$settings = settings;
+// 在所有需要与后台交互的组件中：this.$settings.base_url + '再拼接具体后台路由'
+
+
+
